951e3d4b294afade97c0c0c14418444c86484a8b
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-01-05 17:47:40 +0000

    Create down.py

---
+++ down.py
@@ -0,0 +1,25 @@
+import hexchat
+
+__module_name__ = "DOP Plugin"
+__module_author__ = "ComputerTech"
+__module_version__ = "1.0"
+__module_description__ = "Removes all current status in the current channel when '/down' is typed"
+
+def down_callback(word, word_eol, userdata):
+    channel = hexchat.get_info("channel")
+    prefixes = hexchat.get_prefs("irc_extra_prefixes")
+
+    if "~" in prefixes:
+        hexchat.command("cs deowner " + channel + " " + hexchat.get_info("nick"))
+    elif "&" in prefixes:
+          hexchat.command("cs deprotect " + channel + " " + hexchat.get_info("nick"))
+    elif "@" in prefixes:
+          hexchat.command("cs deop " + channel + " " + hexchat.get_info("nick"))
+    elif "%" in prefixes:
+          hexchat.command("cs dehalfop " + channel + " " + hexchat.get_info("nick"))#
+    elif "+" in prefixes:
+          hexchat.command("cs deop " + channel + " " + hexchat.get_info("nick"))
+    
+    return hexchat.EAT_ALL
+
+hexchat.hook_command("DOWN", down_callback)
