26654ed77539c72f59720313f553149a0f85737a
Colby <60820431+ComputerTech312@users.noreply.github.com>
2023-01-07 19:43:53 +0000

    added -a option

--- slapper.py
+++ slapper.py
@@ -1,10 +1,11 @@
 import hexchat
 import random
+import argparse
 
-__module_name__ = 'Slapper'
+__module_name__ = 'Slap script'
 __module_author__ = 'ComputerTech'
-__module_version__ = '0.1'
-__module_description__ = 'Provides a /slap command for hexchat'
+__module_version__ = '0.2'
+__module_description__ = 'Provides a /slap command for hexchat with an optional -a option to specify the number of slaps'
 
 SLAP_MESSAGES = [
 "slaps {nick} around a bit with a large trout",
@@ -13,26 +14,34 @@ SLAP_MESSAGES = [
 "slaps {nick} with a rolling pin",
 "slaps {nick} with a newspaper",
 "slaps {nick} around a bit with a small trout",
-"slaps {nick} around a bit with a soggy newspaper",
-"slaps {nick} around the face with a baguette",
-"slaps {nick} around a bit with a wet noodle",
-"slaps {nick} around a bit with a foam sword",
-"slaps {nick} around a bit with a banana peel",
-"slaps {nick} with a copy of the IRC rulebook",
-"slaps {nick} with a fresh baguette",
-"slaps {nick} with a pillowy soft roll of bread dough",
-"slaps {nick} with a bundle of herbs and spices",
-"slaps {nick} with a jar of pickles",
+"gives {nick} a swift slap across the face",
+"wallops {nick} with a giant flyswatter",
+"delivers a powerful slap to {nick}'s cheek",
+"slaps {nick} with a wet noodle",
+"sends {nick} flying with a mighty slap",
+"rounds up a group of seagulls to attack {nick} with a series of sharp pecks and slaps",
 ]
 
 def slap_command(word, word_eol, userdata):
+    # If no arguments were provided, display a syntax error message
     if len(word) < 2:
         hexchat.command("HELP slap")
+        hexchat.prnt("Syntax: /slap <target> -a <amount> (-a is optional)")
         return hexchat.EAT_ALL
 
-    target = word[1]
-    message = random.choice(SLAP_MESSAGES).format(nick=target)
-    hexchat.command("me {}".format(message))
+    # Parse the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("nick", help="the nickname of the person to slap")
+    parser.add_argument("-a", "--amount", type=int, default=1, help="the number of times to slap the person")
+    try:
+        args = parser.parse_args(word[1:])
+    except SystemExit:
+        return hexchat.EAT_ALL
+
+    # Send the specified number of slap messages
+    for i in range(args.amount):
+        message = random.choice(SLAP_MESSAGES).format(nick=args.nick)
+        hexchat.command("me {}".format(message))
 
     return hexchat.EAT_ALL
 
