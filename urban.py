173ca573a58397955aa922b644f3c2d8626ddb92
Colby <computertech312@gmail.com>
2024-02-01 11:04:48 +0100

    Update urban.py

--- urban.py
+++ urban.py
@@ -3,40 +3,51 @@ import requests
 
 __module_name__ = 'Urban Dictionary script'
 __module_author__ = 'ComputerTech'
-__module_version__ = '0.3'
+__module_version__ = '0.4'
 __module_description__ = 'Provides a /define command for hexchat which uses the Urban Dictionary API.'
+
+cache = {}
 
 def define(word, word_eol, userdata):
     query = word[1]
-    if "--all" in word:
+    if '--all' in word:
         show_all = True
-        query = query.replace("--all", "").strip()
+        query = query.replace('--all', '').strip()
     else:
         show_all = False
-    r = requests.get("http://api.urbandictionary.com/v0/define?term={}".format(query))
-    if r.status_code != 200:
-        hexchat.prnt("Error: API request failed with status code {}".format(r.status_code))
+
+    if not query:
+        hexchat.prnt('Error: No word provided.')
         return hexchat.EAT_ALL
-    data = r.json()
-    if "error" in data:
-        hexchat.prnt("Error: {}".format(data["error"]))
+
+    if query in cache:
+        data = cache[query]
+    else:
+        r = requests.get(f'http://api.urbandictionary.com/v0/define?term={query}')
+        if r.status_code != 200:
+            hexchat.prnt(f'Error: API request failed with status code {r.status_code}')
+            return hexchat.EAT_ALL
+        data = r.json()
+        cache[query] = data
+
+    if 'error' in data:
+        hexchat.prnt(f'Error: {data["error"]}')
         return hexchat.EAT_ALL
     if show_all:
-        definitions = [entry["definition"] for entry in data["list"]]
-        hexchat.prnt("{}:".format(query))
+        definitions = [entry['definition'] for entry in data['list']]
+        hexchat.prnt(f'{query}:')
         for i, definition in enumerate(definitions):
-            hexchat.prnt("  {}. {}".format(i+1, definition))
+            hexchat.prnt(f'  {i+1}. {definition}')
     else:
-        definition = data["list"][0]["definition"]
-        hexchat.prnt("{}: {}".format(query, definition))
+        definition = data['list'][0]['definition']
+        hexchat.prnt(f'{query}: {definition}')
     
     return hexchat.EAT_ALL
 
-
 def on_unload(userdata):
-    hexchat.prnt("Urban Dictionary script unloaded")
+    hexchat.prnt('Urban Dictionary script unloaded')
 
-hexchat.hook_command("define", define, help="/define <word> [--all] - Looks up the definition of a word using Urban Dictionary. Use --all to display all definitions.")
+hexchat.hook_command('define', define, help='/define <word> [--all] - Looks up the definition of a word using Urban Dictionary. Use --all to display all definitions.')
 hexchat.hook_unload(on_unload)
 
-hexchat.prnt("Urban Dictionary script loaded")
+hexchat.prnt('Urban Dictionary script loaded')
